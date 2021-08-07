bcf7708bdf071bdc44fc15c9cc0828ca70539aae
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-08-07 21:34:18 +0200

    Set scene detection as default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'scene',
+    pathMatch: 'full',
+  },
+  {
     path: 'scene',
     loadChildren: () =>
       import('./modules/scene/scene.module').then((m) => m.SceneModule),

79ee4a03836ff96ddfbb4326d11e7901f6035f09
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-08-08 22:57:23 +0200

    Add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAs--GjtvtvrKgK5XZ3Wxi-fs7ytEzQkoU',
+    authDomain: 'seeing-ai.firebaseapp.com',
+    projectId: 'seeing-ai',
+    storageBucket: 'seeing-ai.appspot.com',
+    messagingSenderId: '315027150120',
+    appId: '1:315027150120:web:0ffa12c7204c385a42ec1d',
+    measurementId: 'G-PQZVJ1N9EE',
+  },
 };
